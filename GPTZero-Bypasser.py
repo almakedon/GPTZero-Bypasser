4e5d5d5ec8292091cfcca76c4f14c9232bf29df6
Jacob Gonzales <122128828+gonzoknows@users.noreply.github.com>
2023-02-02 19:46:34 -0500

    maybe it'll work this time lol

--- GPTZero-Bypasser.py
+++ GPTZero-Bypasser.py
@@ -2,7 +2,15 @@ import random
 
 def replace_letters(file_buffer):
     # Changes all AEOaeo letters with the cyrillic varients.
-    file_buffer = file_buffer.replace(' ', '᲼')
+    file_buffer = file_buffer.replace('a', '𝚊')
+    file_buffer = file_buffer.replace('q', '𝚚')
+    file_buffer = file_buffer.replace('u', '𝚞')
+    file_buffer = file_buffer.replace('n', '𝚗')
+    file_buffer = file_buffer.replace('t', '𝚝')
+    file_buffer = file_buffer.replace('m', '𝚖')
+    file_buffer = file_buffer.replace('c', '𝚌')
+    file_buffer = file_buffer.replace('o', '𝚘')
+    file_buffer = file_buffer.replace('l', '𝚕 ')
     return file_buffer
 
 def insert_zwj(file_buffer):
